64417f5f9c65058ea9f1fdeab4e9f9a144b4978c
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-17 19:05:24 +0200

    Created page components

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-full">
+    <h1>Welcome to the forum</h1>
+    <thread-list :threads="threads"></thread-list>
+  </div>
+</template>
+
+<script>
+import sourceData from "@/data";
+import ThreadList from "../components/ThreadList";
+
+export default {
+  data() {
+    return {
+      threads: Object.values(sourceData.threads),
+      posts: sourceData.posts,
+      users: sourceData.users
+    };
+  },
+  components: {
+    "thread-list": ThreadList
+  }
+};
+</script>
