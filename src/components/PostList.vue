366a4f9bbc41f1e008ae515e24aebcecad2b040d
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-17 21:10:01 +0200

    Created PostList.vue

---
+++ src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="post-list">
+    <post-list-item
+      v-for="post in posts"
+      :key="post['.key']"
+      :post="post"
+    ></post-list-item>
+  </div>
+</template>
+
+<script>
+import PostListItem from "./PostListItem";
+
+export default {
+  components: {
+    "post-list-item": PostListItem
+  },
+  props: {
+    posts: {
+      required: true,
+      type: Array
+    }
+  }
+};
+</script>
+
+<style scoped></style>
