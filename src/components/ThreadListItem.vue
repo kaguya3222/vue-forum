ca62fbce7a2de4238cf2b85c698d74c71f250de1
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-19 01:51:24 +0200

    Refactored "id" param  to "threadId"

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -3,7 +3,7 @@
     <div>
       <p>
         <router-link
-          :to="{ name: 'ThreadShow', params: { id: thread['.key'] } }"
+          :to="{ name: 'ThreadShow', params: { threadId: thread['.key'] } }"
           >{{ thread.title }}</router-link
         >
       </p>
@@ -15,15 +15,6 @@
 
     <div class="activity">
       <p class="replies-count">{{ repliesCount }} replies</p>
-
-      <!--<img class="avatar-medium" src="http://i0.kym-cdn.com/photos/images/facebook/000/010/934/46623-batman_pikachu_super.png" alt="">-->
-
-      <!--<div>-->
-      <!--<p class="text-xsmall">-->
-      <!--<a href="#">Bruce Wayne</a>-->
-      <!--</p>-->
-      <!--<p class="text-xsmall text-faded">2 hours ago</p>-->
-      <!--</div>-->
     </div>
   </div>
 </template>
