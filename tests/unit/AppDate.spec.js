15ff66c4090969a111c6a442b5803bf0eb46cddd
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-21 19:58:54 +0200

    Created specification for AppDate.vue

---
+++ tests/unit/AppDate.spec.js
@@ -0,0 +1,13 @@
+import { mount } from "@vue/test-utils";
+import AppDate from "../../src/components/AppDate";
+
+describe("AppDate", () => {
+  test("Shows date in right format", () => {
+    const wrapper = mount(AppDate, {
+      propsData: {
+        unixDate: 1582302580890
+      }
+    });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
