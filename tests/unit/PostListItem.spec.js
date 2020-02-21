2c34672166c38ac3d482aecc3c9d7fcc43e5b96d
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-21 19:59:20 +0200

    Created specification for PostListItem.vue

---
+++ tests/unit/PostListItem.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount } from "@vue/test-utils";
+import PostListItem from "../../src/components/PostListItem";
+import mockedSourceData from "./mocks/mockedSourceData";
+
+describe("PostListItem", () => {
+  test("Shows post data", () => {
+    const wrapper = shallowMount(PostListItem, {
+      propsData: {
+        post: mockedSourceData.posts["-KsjWehQ--apjDBwSBCZ"]
+      },
+      stubs: {
+        "app-date": true
+      }
+    });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
