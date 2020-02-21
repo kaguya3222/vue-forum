70bac5fda34854e7d46efb6e2b1340965ee2f034
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-21 19:56:33 +0200

    Created mock for sourceData.json

---
+++ tests/unit/mocks/mockedSourceData.js
@@ -0,0 +1,30 @@
+export default {
+  posts: {
+    "-KsjWehQ--apjDBwSBCZ": {
+      edited: {
+        at: 1504037546,
+        by: "ALXhxjwgY9PinwNGHpfai6OWyDu2",
+        moderated: false
+      },
+      publishedAt: 1504035908,
+      text:
+        "I'm curious to know what kind of pets the forum users have. I thought this could be a nice way to get to know each other a little better, so I'll start!\n\nMy dog is a five month old Italian mastiff (Cane Corso). Since I got him from Barcelona I thought a Spanish name would be suitable, and I named him Don Jefe, or Jefe for short (which means \"boss man\").\n\nJefe is a cute, little beast who eats 1.5 kg raw meat every day. He love to fart, snore and play.\n\nHere's Jefe with his Instagram hat and sqeaky ball 🎉🎉🎉\n![Don Jefe](http://i.imgur.com/XCWzR6l.jpg)\n",
+      threadId: "-KsjWehQ--apjDBwSBCY",
+      userId: "ALXhxjwgY9PinwNGHpfai6OWyDu2",
+      ".key": "-KsjWehQ--apjDBwSBCZ"
+    },
+    "-KsjefUiIuNkJieBc5Rq": {
+      edited: {
+        at: 1504038343,
+        by: "FsCDAk9w8NeXEceLV87arpsXjnQ2",
+        moderated: false
+      },
+      publishedAt: 1504038271,
+      text:
+        "I have a Jack Russel Terrier called Milo. He can get a bit crazy some times but I love him.\n\nThere once was [ someone who wanted to put their JRT through plastic surgery](http://www.mirror.co.uk/news/world-news/family-slammed-after-getting-pet-9839609) to look more like Milo. That's crazy and unacceptable!\n\nHere is a picture of him with the mask I bought for Katherine:\n\n![](http://stories.barkpost.com/wp-content/uploads/2014/04/dogmask.jpg)\n\nSometimes, at night, he looks like this when he plays with the mask:\n![](http://www.mobuzz.tv/wp-content/uploads/2014/11/original.jpg)\n",
+      threadId: "-KsjWehQ--apjDBwSBCY",
+      userId: "FsCDAk9w8NeXEceLV87arpsXjnQ2",
+      ".key": "-KsjefUiIuNkJieBc5Rq"
+    }
+  }
+};
