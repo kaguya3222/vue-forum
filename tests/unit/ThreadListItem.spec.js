0170459e6a6abfe1333ad2e2563d97122db6fca0
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-25 20:30:17 +0200

    Created spec for ThreadListItem component

---
+++ tests/unit/ThreadListItem.spec.js
@@ -0,0 +1,18 @@
+import { shallowMount } from "@vue/test-utils";
+import ThreadListItem from "../../src/components/ThreadListItem";
+import mockedSourceData from "./mocks/mockedSourceData";
+
+describe("ThreadListItem", () => {
+  test("Shows thread data", () => {
+    const wrapper = shallowMount(ThreadListItem, {
+      propsData: {
+        thread: mockedSourceData.threads[0]
+      },
+      stubs: {
+        "app-date": true,
+        "router-link": true
+      }
+    });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
