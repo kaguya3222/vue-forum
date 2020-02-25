7967f2454b27cda78d113adb30c1e89db8a09b06
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-26 01:52:29 +0200

    Created CategoryList.vue component

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="forum-list">
+    <category-list-item
+      v-for="category in categories"
+      :key="category['.key']"
+      :category="category"
+    />
+  </div>
+</template>
+
+<script>
+import CategoryListItem from "./CategoryListItem";
+export default {
+  name: "CategoryList.vue",
+  components: {
+    "category-list-item": CategoryListItem
+  },
+  props: {
+    categories: {
+      required: true,
+      type: Array
+    }
+  }
+};
+</script>
+
+<style scoped></style>
