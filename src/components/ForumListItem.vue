ae94b454c76b94f32c356af7079470d88096d3ba
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-25 21:46:53 +0200

    Created ForumListItem.vue

---
+++ src/components/ForumListItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="forum-listing">
+    <div class="forum-details">
+      <router-link
+        class="text-xlarge"
+        :to="{ name: 'Forum', params: { id: forum['.key'] } }"
+      >
+        {{ forum.name }}
+      </router-link>
+      <p>{{ forum.description }}</p>
+    </div>
+
+    <div class="threads-count">
+      <p class="count">{{ threadsCount }}</p>
+      {{ threadsCountWord }}
+    </div>
+
+    <div class="last-thread">
+      <!-- TODO: implement later -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    forum: {
+      required: true,
+      type: Object
+    }
+  },
+  name: "ForumListItem",
+  computed: {
+    threadsCount() {
+      return this.forum.threads ? Object.values(this.forum.threads).length : 0;
+    },
+    threadsCountWord() {
+      return this.threadsCount === 1 ? "thread" : "threads";
+    }
+  }
+};
+</script>
+
+<style scoped></style>
