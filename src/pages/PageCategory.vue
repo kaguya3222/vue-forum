bc50ce4e6e83771788ce37981d061f110150188f
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-26 01:53:00 +0200

    Created PageCategory.vue component

---
+++ src/pages/PageCategory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-full col-full_centered">
+    <div class="col-full push-top">
+      <h1>{{ category.name }}</h1>
+    </div>
+    <category-list-item :category="category" />
+  </div>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+import CategoryListItem from "../components/CategoryListItem";
+export default {
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+  name: "PageCategory",
+  components: { "category-list-item": CategoryListItem },
+  computed: {
+    category() {
+      return sourceData.categories[this.id];
+    }
+  }
+};
+</script>
+
+<style scoped></style>
