ef1c3bf255df8584a12b34b096eeb2919a54bfdb
kaguya3222 <maksimanisimov322@gmail.com>
2020-02-27 03:01:41 +0200

    Writed logic for adding post in index.js vuex module

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,27 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: sourceData,
-  mutations: {},
-  actions: {},
+  mutations: {
+    setPost(state, { post, postId }) {
+      Vue.set(state.posts, postId, post);
+    },
+    appendPostToThread(state, { postId, threadId }) {
+      const thread = state.threads[threadId];
+      Vue.set(thread.posts, postId, postId);
+    },
+    appendPostToUser(state, { postId, userId }) {
+      const user = state.users[userId];
+      Vue.set(user.posts, postId, postId);
+    }
+  },
+  actions: {
+    createPost(store, post) {
+      const postId = "greatPost" + Math.random();
+      post[".key"] = postId;
+      store.commit("setPost", { post, postId });
+      store.commit("appendPostToThread", { postId, threadId: post.threadId });
+      store.commit("appendPostToUser", { postId, userId: post.userId });
+    }
+  },
   modules: {}
 });
