de9a127194fb6cc2a2dd457ea1f0f94596d683b3
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-01 01:43:07 +0200

    Created TheNavbar.vue single-instance component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
+    <nav-bar />
     <div class="container">
       <router-view />
     </div>
   </div>
 </template>
+
+<script>
+import TheNavbar from "./components/TheNavbar";
+export default {
+  components: {
+    "nav-bar": TheNavbar
+  }
+};
+</script>
 
 <style>
 @import "./assets/css/style.css";
