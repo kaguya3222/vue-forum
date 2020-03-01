5b0c8edd052dd7cd3f4f9c8e665b7f22ae8aa227
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-01 21:51:37 +0200

    Added new tests for PageProfile spec

--- tests/unit/pages-specs/PageProfile.spec.js
+++ tests/unit/pages-specs/PageProfile.spec.js
@@ -2,8 +2,11 @@ import Vuex from "vuex";
 import { createLocalVue, shallowMount } from "@vue/test-utils";
 import PageProfile from "../../../src/pages/PageProfile";
 import PostList from "../../../src/components/PostList";
-import sourceStore from "../../../src/store/index";
-import getters from "../../../src/store/getters";
+import UserProfileCard from "../../../src/components/UserProfileCard";
+import UserProfileCardEditor from "../../../src/components/UserProfileCardEditor";
+import postsStore from "@/store/modules/posts/store";
+import userStore from "../../../src/store/modules/users/store";
+import getters from "../../../src/store/modules/users/getters";
 import mockedSourceData from "../mocks/mockedSourceData";
 import { countObjectProperties } from "../../../src/helpers";
 
@@ -12,7 +15,10 @@ localVue.use(Vuex);
 
 describe("PageProfile", () => {
   const store = new Vuex.Store({
-    state: sourceStore.state,
+    state: {
+      ...postsStore.state,
+      ...userStore.state
+    },
     getters: {
       ...getters
     }
@@ -23,26 +29,35 @@ describe("PageProfile", () => {
   });
   store.state.authId = "VXjpr2WHa8Ux4Bnggym8QFLdv5C3";
 
-  test("Shows user information", () => {
+  test("Shows users information", () => {
     expect(wrapper).toMatchSnapshot();
   });
   test("Accepts PostList as a child component", () => {
     expect(wrapper.contains(PostList)).toBe(true);
   });
-  test("Accepts vuex authUser getter", () => {
-    expect(wrapper.vm.user).toEqual(getters.authUser(sourceStore.state));
+  test("Accepts UserProfileCard as a child component", () => {
+    expect(wrapper.contains(UserProfileCard)).toBe(true);
   });
-  test("Creates userPosts array of certain user", () => {
+  test("Accepts UserProfileCardEditor as a child component when 'edit' property is true", () => {
+    wrapper.setProps({ edit: true });
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.contains(UserProfileCardEditor)).toBe(true);
+    });
+  });
+  test("Accepts vuex authUser getter", () => {
+    expect(wrapper.vm.user).toEqual(getters.authUser(userStore.state));
+  });
+  test("Creates userPosts array of certain users", () => {
     const post = mockedSourceData.posts["-KsjnAAjzfpznUVh4DSa"];
     const hasPost = wrapper.vm.userPosts.some(
       userPost => userPost[".key"] === post[".key"]
     );
     expect(hasPost).toBe(true);
   });
-  test("Correctly shows user posts number", () => {
+  test("Correctly shows users posts number", () => {
     expect(wrapper.vm.userPostsCount).toBe(wrapper.vm.userPosts.length);
   });
-  test("Correctly shows user threads number", () => {
+  test("Correctly shows users threads number", () => {
     const userThreadsNumber = countObjectProperties(wrapper.vm.user.threads);
     expect(wrapper.vm.userThreadsCount).toBe(userThreadsNumber);
   });
