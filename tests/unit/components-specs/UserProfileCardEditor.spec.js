198708a9e71287a3511d9cab24fa599c4f18a610
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-02 02:54:26 +0200

    Created spec for UserProfileCardEditor component

---
+++ tests/unit/components-specs/UserProfileCardEditor.spec.js
@@ -0,0 +1,69 @@
+import { mount, createLocalVue } from "@vue/test-utils";
+import UserProfileCardEditor from "../../../src/components/UserProfileCardEditor";
+import mockedSourceData from "../mocks/mockedSourceData";
+import { countObjectProperties } from "../../../src/helpers";
+import rootStore from "@/store/";
+import Vuex from "vuex";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe("UserProfileCardEditor", () => {
+  const store = new Vuex.Store({
+    state: rootStore.state
+  });
+  const $router = {
+    push: jest.fn()
+  };
+  const user = mockedSourceData.users[0];
+  const wrapper = mount(UserProfileCardEditor, {
+    propsData: {
+      user,
+      userPostsCount: countObjectProperties(user.posts),
+      userThreadsCount: countObjectProperties(user.threads)
+    },
+    mocks: {
+      $router
+    },
+    localVue,
+    store
+  });
+  wrapper.vm.$store.dispatch = jest.fn();
+  test("Correctly shows form for editing user data", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  test("Calls 'save' method when 'Edit' button is clicked", () => {
+    const saveMethodSpy = jest.spyOn(wrapper.vm, "save");
+    const editButton = wrapper.find(".btn-blue");
+    editButton.trigger("click");
+    expect(saveMethodSpy).toHaveBeenCalled();
+  });
+  test("Calls 'cancel' method when 'Cancel' button is clicked", () => {
+    const cancelMethodSpy = jest.spyOn(wrapper.vm, "cancel");
+    const cancelButton = wrapper.find(".btn-ghost");
+    cancelButton.trigger("click");
+    expect(cancelMethodSpy).toHaveBeenCalled();
+  });
+  describe("'save' method", () => {
+    beforeEach(() => {
+      const editButton = wrapper.find(".btn-blue");
+      editButton.trigger("click");
+    });
+    test("dispatches 'updateUser' action", () => {
+      expect(wrapper.vm.$store.dispatch).toHaveBeenCalledWith(
+        "updateUser",
+        expect.any(Object)
+      );
+    });
+    test("Moves user to 'Profile' route", () => {
+      expect(wrapper.vm.$router.push).toHaveBeenCalledWith({ name: "Profile" });
+    });
+  });
+  describe("'cancel' method", () => {
+    test("Moves user to 'Profile' route", () => {
+      const cancelButton = wrapper.find(".btn-ghost");
+      cancelButton.trigger("click");
+      expect(wrapper.vm.$router.push).toHaveBeenCalledWith({ name: "Profile" });
+    });
+  });
+});
