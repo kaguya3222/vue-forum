43dee7dadfd7c80685ef32d9dcaaa3babec1ce75
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-02 02:54:01 +0200

    Created spec for UserProfileCard

---
+++ tests/unit/components-specs/UserProfileCard.spec.js
@@ -0,0 +1,19 @@
+import { mount } from "@vue/test-utils";
+import UserProfileCard from "../../../src/components/UserProfileCard";
+import mockedSourceData from "../mocks/mockedSourceData";
+import { countObjectProperties } from "../../../src/helpers";
+
+describe("UserProfileCard", () => {
+  const user = mockedSourceData.users[0];
+  const wrapper = mount(UserProfileCard, {
+    propsData: {
+      user,
+      userPostsCount: countObjectProperties(user.posts),
+      userThreadsCount: countObjectProperties(user.threads)
+    },
+    stubs: ["router-link"]
+  });
+  test("Correctly shows user profile card data", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
