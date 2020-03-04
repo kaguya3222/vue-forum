a44b474a13c2b9cf3872e04e0ec5ccdf5d7121c5
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-04 23:33:33 +0200

    Created specs for PageThreadCreate component

---
+++ tests/unit/pages-specs/PageThreadCreate.spec.js
@@ -0,0 +1,56 @@
+import { createLocalVue, mount } from "@vue/test-utils";
+import PageThreadCreate from "../../../src/pages/PageThreadCreate";
+import rootStore from "@/store/";
+import Vuex from "vuex";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe("PageThreadCreate", function() {
+  const store = new Vuex.Store({
+    state: rootStore.state
+  });
+  store.dispatch = jest.fn();
+  const $router = {
+    push: jest.fn()
+  };
+  const wrapper = mount(PageThreadCreate, {
+    propsData: {
+      forumId: "-KpOx5Y4AqRr3sB4Ybwj"
+    },
+    mocks: {
+      $router
+    },
+    localVue,
+    store
+  });
+  test("Correctly shows thread creation form", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  describe("save", () => {
+    const threadTitleInput = wrapper.find("#thread_title");
+    const threadContentInput = wrapper.find("#thread_content");
+    const threadCreationForm = wrapper.find("form");
+    beforeEach(() => {
+      threadTitleInput.element.value = "Thread title";
+      threadTitleInput.trigger("input");
+      threadContentInput.element.value = "Thread content";
+      threadContentInput.trigger("input");
+      threadCreationForm.trigger("submit");
+    });
+    test("Dispatches 'createThread' action after form is submitted", () => {
+      expect(store.dispatch).toHaveBeenCalledWith("createThread", {
+        forumId: wrapper.vm.forum[".key"],
+        title: wrapper.vm.title,
+        text: wrapper.vm.text
+      });
+    });
+    test("Redirects user to 'ThreadShow' route after form is submitted", () => {
+      store.dispatch = jest.fn().mockResolvedValue("Thread id");
+      expect($router.push).toHaveBeenCalledWith({
+        name: "ThreadShow",
+        params: expect.any(Object)
+      });
+    });
+  });
+});
