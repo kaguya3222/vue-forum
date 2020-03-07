7dac2d69b8ef7e7ad54001c66d23cbd65170261b
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-07 04:25:26 +0200

    Created PageThreadCreate component

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -3,57 +3,22 @@
     <h1>
       Create new thread in <i>{{ forum.name }}</i>
     </h1>
-
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input
-          v-model="title"
-          type="text"
-          id="thread_title"
-          class="form-input"
-          name="title"
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          v-model="text"
-          id="thread_content"
-          class="form-input"
-          name="content"
-          rows="8"
-          cols="140"
-        ></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button class="btn btn-ghost" @click="cancel" type="button">
-          Cancel
-        </button>
-        <button class="btn btn-blue" type="submit" name="Publish">
-          Publish
-        </button>
-      </div>
-    </form>
+    <thread-editor @save="save" @cancel="cancel" />
   </div>
 </template>
 
 <script>
+import ThreadEditor from "../components/ThreadEditor";
 export default {
   name: "PageThreadCreate",
+  components: {
+    "thread-editor": ThreadEditor
+  },
   props: {
     forumId: {
       required: true,
       type: String
     }
-  },
-  data() {
-    return {
-      text: "",
-      title: ""
-    };
   },
   computed: {
     forum() {
@@ -61,11 +26,11 @@ export default {
     }
   },
   methods: {
-    async save() {
+    async save({ text, title }) {
       const threadId = await this.$store.dispatch("createThread", {
         forumId: this.forum[".key"],
-        title: this.title,
-        text: this.text
+        title: title,
+        text: text
       });
       await this.$router.push({ name: "ThreadShow", params: { threadId } });
     },
