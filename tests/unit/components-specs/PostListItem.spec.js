2804e7b9d5632cad8f00e1a2a646d05af849aedc
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-08 02:48:43 +0200

    Added test in PostListItem spec

--- tests/unit/components-specs/PostListItem.spec.js
+++ tests/unit/components-specs/PostListItem.spec.js
@@ -4,6 +4,7 @@ import PostListItem from "../../../src/components/PostListItem";
 import mockedSourceData from "../mocks/mockedSourceData";
 import sourceStore from "../../../src/store/index";
 import userGetters from "@/store/modules/users/getters";
+import PostEditor from "../../../src/components/PostEditor";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -15,17 +16,20 @@ describe("PostListItem", () => {
       ...userGetters
     }
   });
+  const wrapper = shallowMount(PostListItem, {
+    propsData: {
+      post: mockedSourceData.posts["-KsjWehQ--apjDBwSBCZ"]
+    },
+    stubs: {
+      "app-date": true
+    },
+    localVue,
+    store
+  });
   test("Shows post data in posts list", () => {
-    const wrapper = shallowMount(PostListItem, {
-      propsData: {
-        post: mockedSourceData.posts["-KsjWehQ--apjDBwSBCZ"]
-      },
-      stubs: {
-        "app-date": true
-      },
-      localVue,
-      store
-    });
     expect(wrapper).toMatchSnapshot();
+  });
+  test("Accepts PostEditor as a child component", () => {
+    expect(wrapper.contains(PostEditor)).toBe(true);
   });
 });
