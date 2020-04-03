928c22fdb372f0cfa7fd69b5003868bbea82b2e0
kaguya3222 <maksimanisimov322@gmail.com>
2020-04-03 20:11:59 +0300

    Installed firebase realtime database

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import firebase from "firebase";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -7,6 +8,17 @@ import AppDate from "./components/AppDate";
 
 Vue.config.productionTip = false;
 Vue.component("app-date", AppDate);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5-pR1GgnFtdkImT9F-LKxQ9BrtAjc488",
+  authDomain: "vue-forum-a237f.firebaseapp.com",
+  databaseURL: "https://vue-forum-a237f.firebaseio.com",
+  projectId: "vue-forum-a237f",
+  storageBucket: "vue-forum-a237f.appspot.com",
+  messagingSenderId: "630364465710",
+  appId: "1:630364465710:web:57bad9757fbe78707c8dbf"
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
