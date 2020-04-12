b9b3b2b41891302c59e42624427afc4decb6553d
kaguya3222 <maksimanisimov322@gmail.com>
2020-04-12 21:17:06 +0300

    Created userPosts getter in users vuex module

--- src/store/modules/users/getters.js
+++ src/store/modules/users/getters.js
@@ -2,6 +2,12 @@ import { countObjectProperties } from "../../../helpers";
 
 export default {
   authUser: state => state.items[state.authId],
+  userPosts: (state, getters, rootState) => id => {
+    const user = state.items[id];
+    return user.posts
+      ? Object.values(rootState.posts.items).filter(post => post.userId === id)
+      : [];
+  },
   items: state => state.items,
   countUserPosts: state => id => countObjectProperties(state.items[id].posts),
   countUserThreads: state => id =>
